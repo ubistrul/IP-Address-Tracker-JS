a255ce13b2ea66271ec37a72dd848ed5f3622ecd
Alexey Serduchenko <ubistrul@gmail.com>
2024-03-19 00:09:01 +0200

    Basic HTML + CSS
    Added basic HTML and CSS code. Starting write a Javasript.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-import './../scss/main.scss';
+const IP_APIKEY = `at_NUCWwHBl2qoJCSb7WwHnW270ooBzq`;
+
+const IP_URL_MAIN = `https://geo.ipify.org/api/v2/country,city`;
+const IP_URL_APIPART = `?apiKey=`;
+const IP_URL_ENDPART = `&ipAddress=`;
+const IP_URL_FULL = IP_URL_MAIN + IP_URL_APIPART + IP_APIKEY + IP_URL_ENDPART;
+
+const ipEl = document.getElementById('ip');
+const locationEl = document.getElementById('location');
+const timezoneEl = document.getElementById('timezone');
+const ispEl = document.getElementById('isp');
+
+document.addEventListener('DOMContentLoaded', getData);
+
+function setText(el, text) {
+    el.textContent = text;
+}
